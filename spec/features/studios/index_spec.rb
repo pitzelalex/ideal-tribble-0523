a98b116cbbfcf48146a18a5c3b0e8d7769c60aa3
Alex Pitzel <pitzelalex@gmail.com>
2023-01-02 12:15:55 -0500

    test: Create studio index lists movies test

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -23,7 +23,32 @@ RSpec.describe 'studios index' do
       expect(page).to have_content(studio1.location)
       expect(page).to have_content(studio2.name)
       expect(page).to have_content(studio2.location)
+    end
+  end
 
+  it 'lists all movies for each studio under each studios entry' do
+    studio1 = Studio.create!(name: 'Universal', location: 'Hollywood')
+    studio2 = Studio.create!(name: 'Herne Hill', location: 'Toronto')
+    movie1 = studio1.movies.create!(title: 'Jurassic Park', creation_year: 1993, genre: 'Action')
+    movie2 = studio1.movies.create!(title: 'The Fast and the Furious', creation_year: 2001, genre: 'Crime/Thriller')
+    movie3 = studio2.movies.create!(title: 'Desert Warrior', creation_year: 2021, genre: 'Action/Adventure')
+
+    visit '/studios'
+
+    
+    within "#studio-#{studio1.id}" do
+      expect(page).to have_content(movie1.title)
+      expect(page).to have_content(movie1.creation_year)
+      expect(page).to have_content(movie1.genre)
+      expect(page).to have_content(movie2.title)
+      expect(page).to have_content(movie2.creation_year)
+      expect(page).to have_content(movie2.genre)
+    end
+
+    within "#studio-#{studio2.id}" do
+      expect(page).to have_content(movie3.title)
+      expect(page).to have_content(movie3.creation_year)
+      expect(page).to have_content(movie3.genre)
     end
   end
 end
